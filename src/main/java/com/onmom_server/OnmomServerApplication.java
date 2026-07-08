6a23fe64b4ee930aab7175005cffca1e9208ac89
chjh1211 <chjh8800@naver.com>
2026-07-08 15:29:36 +0900

    git commit -m "초기 세팅 완료"

---
+++ src/main/java/com/onmom_server/OnmomServerApplication.java
@@ -0,0 +1,13 @@
+package com.onmom_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnmomServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnmomServerApplication.class, args);
+	}
+
+}
